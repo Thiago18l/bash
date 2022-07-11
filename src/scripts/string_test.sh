6c24d6eb6d0f0d01af3cfd4d3465f1c5753ca474
Thiago Lopes <thiago.lopes.dev@gmail.com>
2022-07-10 23:13:55 -0300

    chore: add string tests

--- src/scripts/string_test.sh
+++ src/scripts/string_test.sh
@@ -7,3 +7,22 @@ fi
 [ -z "" ] && echo "EMPTY" # -z tests if a string is empty
 
 [ -n "test" ] && echo "IS NOT EMPTY" # -n tests if a string is not empty
+
+
+function isFalse() {
+  local arg=$1
+  if [[ $arg -eq 1 ]]; then
+    echo FALSE
+    return 1
+  else
+    echo TRUE
+    return 0
+  fi
+}
+
+str1=abc
+str2=def
+
+[ "${str1}" \< "${str2}" ] && echo "LESS THAN IS $(isFalse $?)"
+
+[ "${str1}" \> "${str2}" ] || echo "GREATER THAN IS $(isFalse $?) "  # false
