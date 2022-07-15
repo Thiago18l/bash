ef9a12dcfe1baced65b9757d14915a7665f0826c
Thiago Lopes <thiago.lopes.dev@gmail.com>
2022-07-15 15:11:15 -0300

    chore: pathname

--- src/scripts/command_subs.sh
+++ src/scripts/command_subs.sh
@@ -5,3 +5,13 @@ source src/scripts/redirect.sh
 
 FILE=$( find . -type f -name arithmetic.sh)
 wc -l $FILE > $OUTPUT_PATH/$( date +%d-%m-%Y ).log
+
+
+## Pathname expansion
+
+context=$( find . -type d -name src )
+echo ${context}
+
+echo *c
+
+echo *d
